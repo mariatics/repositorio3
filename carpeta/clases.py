fa88ffd1c8fc7d44ecef5807f1464495025dbdbe
Maria Guadalupe Gress Ugarte <22011989@itsoeh.edu.mx>
2024-06-23 21:06:18 -0600

    se agrego clases

--- carpeta/clases.py
+++ carpeta/clases.py
@@ -1,1 +1,11 @@
-
+def frec_abs(datos_entrada):
+    datos_entrada.sort()
+    clases, fa_absoluta = [], []
+    for elemento in datos_entrada:
+        if elemento not in clases:
+            clases.append(elemento)
+            fa_absoluta.append(1)
+        else:        
+            idx = clases.index(elemento)        
+            fa_absoluta[idx] += 1
+    return clases, fa_absoluta  
